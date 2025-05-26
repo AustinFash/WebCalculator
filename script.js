12f65238fe3085a0d8d7aad71b9fbba8f93becc2
AustinFash <asf128@scarletmail.rutgers.edu>
2025-05-25 22:19:41 -0400

    Updated to accept longer numbers

--- script.js
+++ script.js
@@ -17,45 +17,45 @@ let btnequal = document.querySelector("#btnequal")
 let display = document.querySelector(".display")
 
 let operand = ""
-let num1 = null
-let num2 = null
+let num1 = ""
+let num2 = ""
 display.textContent = ""
 
 
 btn1.addEventListener('click',function() {
-    setNum(1)
+    setNum("1")
 });
 
 btn2.addEventListener('click',function() {
-     setNum(2)
+     setNum("2")
 });
 
 btn3.addEventListener('click',function() {
-     setNum(3)
+     setNum("3")
 });
 
 btn4.addEventListener('click',function() {
-     setNum(4)
+     setNum("4")
 });
 
 btn5.addEventListener('click',function() {
-    setNum(5)
+    setNum("5")
 });
 
 btn6.addEventListener('click',function() {
-    setNum(6)
+    setNum("6")
 });
 
 btn7.addEventListener('click',function() {
-     setNum(7)
+     setNum("7")
 });
 
 btn8.addEventListener('click',function() {
-     setNum(8)
+     setNum("8")
 });
 
 btn9.addEventListener('click',function() {
-     setNum(9)
+     setNum("9")
 });
 
 btnminus.addEventListener('click',function() {
@@ -93,40 +93,37 @@ btnclear.addEventListener('click',function() {
     display.textContent = ""
 });
 btnequal.addEventListener('click',function() {
-    num1 = operate(num1,operand,num2)
-    num2 = null;
+    num1 = operate(num1,operand,num2).toString()
+    num2 = "";
     operand = "";
 });
 
 function setNum(num){
-    if(num1 == null){
-        num1 = num
+    if(operand == ""){
+        num1 += num
         display.textContent += num
-    }else if (operand == ""){
-        alert("Please set a operand before adding another value")
-    }else if (num2 == null){
-        num2 = num
+    }else{
+        num2 += num
         display.textContent += num
     }
-        
 }
 
 function operate(num1,operand,num2){
     if(operand == "+"){
-        display.textContent = num1 + num2
-         return num1 + num2
+        display.textContent = +num1 + +num2;
+        return +num1 + +num2;
     }else if(operand == "-"){
-        display.textContent = num1 - num2
-        return num1 - num2
+        display.textContent = +num1 - +num2;
+        return +num1 - +num2;
     }else if(operand == "*"){
-        display.textContent = num1 * num2
-       return num1 * num2
+        display.textContent = +num1 * +num2;
+       return +num1 * +num2;
     }else if(operand == "/"){
         if(num2 == 0){
             display.textContent = "undefined"
         }else{
-             display.textContent = Math.round((num1 / num2) * 10000) /10000
-             return Math.round((num1 / num2) * 10000) /10000
+             display.textContent = Math.round((+num1 / +num2) * 10000) /10000
+             return Math.round((+num1 / +num2) * 10000) /10000
         }
     }else{
         alert("Operand was undefined")
